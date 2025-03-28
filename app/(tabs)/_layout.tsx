e3299537ec2d51c8e430e73528ca53d751ffac56
Gaurav Panchal <gkp.125@gmail.com>
2025-03-28 18:35:54 +0530

    Remove unused TabBarIcon component from TabLayout for cleaner code.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -42,14 +42,6 @@ const ErrorFallback = ({
   </View>
 );
 
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>["name"];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
-
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   const router = useRouter();
